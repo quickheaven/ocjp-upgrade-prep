43353d714b05c1b54b1366298696181e7ee2d429
Arjie Cristobal <arjie_c82@yahoo.com>
2019-03-14 20:22:00 -0400

    Initial project setup.

---
+++ ocjp-core/src/test/java/ca/quickheaven/ocjp/OcjpCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package ca.quickheaven.ocjp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OcjpCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
