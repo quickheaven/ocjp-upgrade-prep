d7dc50c45816050b0ecce8de750e6ef632c02510
arjie cristobal <arjie_cristobal@yahoo.com>
2019-06-01 21:28:41 -0400

    Reviewing Exceptions.

---
+++ ocjp-core/src/main/java/com/quickheaven/ocjp/exceptions/Auto.java
@@ -0,0 +1,33 @@
+package com.quickheaven.ocjp.exceptions;
+
+/**
+ * Resources are closed in the reverse order from which they were created.
+ * 
+ * Result: Close: 2 Close: 1 ex finally
+ * 
+ * @author frozencloud
+ *
+ */
+public class Auto implements AutoCloseable {
+	int num;
+
+	Auto(int num) {
+		this.num = num;
+	}
+
+	public void close() {
+		System.out.println("Close: " + num);
+	}
+
+	public static void main(String[] args) {
+		try (Auto a1 = new Auto(1); Auto a2 = new Auto(2)) {
+			throw new RuntimeException();
+		} catch (Exception e) {
+			System.out.println("ex");
+		} finally {
+			System.out.println("finally");
+		}
+
+	}
+
+}
---
+++ ocjp-core/src/main/java/com/quickheaven/ocjp/exceptions/JammedTurkeyCage.java
@@ -0,0 +1,18 @@
+package com.quickheaven.ocjp.exceptions;
+
+public class JammedTurkeyCage implements AutoCloseable {
+
+	public void close() throws IllegalStateException {
+		//throw new IllegalStateException("Cage door does not close");
+		throw new RuntimeException("turkeys ran off");
+	}
+
+	public static void main(String[] args) {
+		try (JammedTurkeyCage t = new JammedTurkeyCage()) {
+			System.out.println("put turkeys in");
+		} catch (IllegalStateException e) {
+			System.out.println("caught: " + e.getMessage());
+		}
+	}
+
+}
