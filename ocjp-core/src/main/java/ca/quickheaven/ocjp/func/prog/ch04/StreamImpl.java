ddcab81985998b081adaf85f4c8e332a06ce1abb
quickheaven <quckheaven@gmail.com>
2019-07-11 22:11:54 -0400

    Using Common Terminal Operations (final)

--- ocjp-core/src/main/java/ca/quickheaven/ocjp/func/prog/ch04/StreamImpl.java
+++ ocjp-core/src/main/java/ca/quickheaven/ocjp/func/prog/ch04/StreamImpl.java
@@ -1,9 +1,10 @@
 package ca.quickheaven.ocjp.func.prog.ch04;
 
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamImpl {
@@ -55,9 +56,97 @@ public class StreamImpl {
         // The findAny() and findFirst() methods return an element of the stream unless the stream is empty. If the stream
         // is empty, they return an empty Optional.
         // This example finds an animal:
-        Stream<String> s = Stream.of("monkey", "gorilla", "bonobo");
+        Stream<String> sFind = Stream.of("monkey", "gorilla", "bonobo");
         Stream<String> infinite = Stream.generate(() -> "chimp");
-        s.findAny().ifPresent(System.out::println); // monkey
+        sFind.findAny().ifPresent(System.out::println); // monkey
         infinite.findAny().ifPresent(System.out::println); // chimp
+
+        // allMatch(), anyMatch() and nonMatch()
+        // The allMatch(), anyMatch() and noneMatch() methods search a stream and return information about how the stream
+        // pertains to the predicate.
+        List<String> listMatch = Arrays.asList("monkey", "2", "chimp");
+        Stream<String> infiniteMatch = Stream.generate(() -> "chimp");
+        Predicate<String> pred = x -> Character.isLetter(x.charAt(0));
+        System.out.println(listMatch.stream().anyMatch(pred)); // true
+        System.out.println(listMatch.stream().allMatch(pred)); // false
+        System.out.println(listMatch.stream().noneMatch(pred)); // false
+        System.out.println(listMatch.stream().anyMatch(pred)); // true
+
+        // Remember that allMatch(), anyMatch() and noneMatch() return a boolean. By contrast, the find methods return an
+        // Optional because they return an element of the stream.
+
+
+        // forEach()
+        Stream<String> sForEach = Stream.of("Monkey", "Gorilla", "Bonobo");
+        sForEach.forEach(System.out::print); // MonkeyGorillaBonobo
+
+
+        // reduce()
+        // The reduce() method combines a stream into a single object.
+        // before lambda:
+        String[] array = new String[]{"w", "o", "l", "f"};
+        String result = "";
+        for (String s : array) result = result + s;
+        System.out.println(result); // wolf
+
+        // with lambda
+        Stream<String> stream = Stream.of("w", "o", "l", "f");
+        String word = stream.reduce("", (s, c) -> s + c);
+        System.out.println(word); // wolf
+
+        // with lambda (rewrite)
+        stream = Stream.of("w", "o", "l", "f");
+        word = stream.reduce("", String::concat);
+        System.out.println(word); // wolf
+
+        // Can you write a reduction to multiply all of the Integer objects in a stram?
+        Stream<Integer> stream1 = Stream.of(3, 5, 6);
+        System.out.println(stream1.reduce(1, (a, b) -> a * b)); // 90
+
+        // (a) If the stream is empty, an empty Optional is returned.
+        // (b) If the stream has one element, it is returned.
+        // (c) If the stream has multiple elements, the accumulator is applied to combine them
+        // The following illustrates each of these scenarios:
+        BinaryOperator<Integer> op = (a, b) -> a * b;
+        Stream<Integer> empty = Stream.empty();
+        Stream<Integer> oneElement = Stream.of(3);
+        Stream<Integer> threeElements = Stream.of(3, 5, 6);
+
+        empty.reduce(op).ifPresent(System.out::println); // no output (a)
+        oneElement.reduce(op).ifPresent(System.out::println); // 3 (b)
+        threeElements.reduce(op).ifPresent(System.out::println); // 90 (c)
+
+
+        // collect()
+        // The collect() method is a special type of reduction called a mutable reduction. It is more efficient that a
+        // regular reduction because we use the same mutable object while accumulating.
+
+        // The first parameter is a Supplier that creates the object that will store the results as we collect data.
+        // Remember that a Supplier doesnt take any parameters and return a value. In this case, it constructs a new StringBuilder
+        // The second parameter is a BiConsumer, which takes two parameter and doesnt return anything. It is responsible
+        // for adding one more element to the data collection. In this example, it appends the next String to the StringBuilder.
+        // the final parameter is another BiConsumer. It is responsible for taking two data collections and merging it.
+        stream = Stream.of("w", "o", "l", "f");
+        StringBuilder wordSb = stream.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
+        System.out.println(wordSb); // wolf
+
+        // Example where the logic is different in the accumulator and combiner.
+        // The collector has three parts as before. The supplier creates an empty TreeSet. The accumulator add a single
+        // String from the Stream to the TreeSet. The combiner adds all of the elements of one TreeSet to another in case
+        // the operations were done in parallel and need to be merged.
+        stream = Stream.of("w", "o", "l", "f");
+        TreeSet<String> set1 = stream.collect(TreeSet::new, TreeSet::add, TreeSet::addAll);
+        System.out.println(set1); // [f, l, o, w]
+
+        // we can rewrite the previous example as follows:
+        stream = Stream.of("w", "o", "l", "f");
+        TreeSet<String> set2 = stream.collect(Collectors.toCollection(TreeSet::new));
+        System.out.println(set2); // [f, l, o, w]
+
+        // If we didnt need the set to be sorted, we could make the code even shorter:
+        stream = Stream.of("w", "o", "l", "f");
+        Set<String> set3 = stream.collect(Collectors.toSet());
+        System.out.println(set3); // [f, w, l, o]
+
     }
 }
